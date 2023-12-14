22e5893f3f7c6a89256ffd0455fc52bc49c585cc
Fatimajavid <73313402+Fatimajavid@users.noreply.github.com>
2023-12-14 12:24:31 -0500

    Update Home.py

--- frontend/Home.py
+++ frontend/Home.py
@@ -1,12 +1,12 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="Anote Financial Chatbot",
+    page_title="ANOTE Financial Chatbot",
     page_icon="images/anote_ai_logo.png",
 )
-st.header("Anote Financial Chatbot :speech_balloon:")
-st.subheader("Welcome to Anote's Finance Chatbot!")
-st.success("This is a chatbot catered to data teams within the finance sector to answer questions on documents such as 10-Ks.\n\n Chat about your documents while keeping your data secure. ")
+st.header("ANOTE Financial Chatbot :speech_balloon:")
+st.subheader("Welcome to ANOTE!")
+st.success("This is our Private GPT. We aim to help data teams within the finance sector to answer questions on documents such as 10-Ks.\n\n Come chat about your documents while keeping your data private and secure. ")
 st.info("You can select between either of the available options: ")
 
 # Create three columns 
@@ -14,7 +14,7 @@ col1, col2 = st.columns([1,1])
 
 with col1:
     img = st.image('images/apichat.png')
-    st.markdown('<a href="/EdgarAPIChatbot"> </a>', unsafe_allow_html=True)
+    st.markdown('<a href="/EdgarAPIChatbot"></a>', unsafe_allow_html=True)
     st.warning("Specify the ticker of a company to ask questions based on the 10K of the company.")
 
 
